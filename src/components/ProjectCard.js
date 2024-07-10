905f495ff9008ca358dc67f2cb1e8859c6ad1462
Shubhayan Srivastava <88150274+shubhayan935@users.noreply.github.com>
2024-07-10 22:28:16 +0530

    Update ProjectCard.js

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,7 +6,9 @@ const ProjectCard = ({ title, image, description, highlight, tech, link }) => (
       <h2 className="project-title">{title}</h2>
       <div className="project-thumbnail" style={{ backgroundImage: `url('${image}')` }}></div>
       <p className="project-description">{description}</p>
-      <p className="project-highlight">{highlight}</p>
+      {highlight && (
+        <p className="project-highlight">{highlight}</p>
+      )}
       <div className="project-tech">
         {tech.map((item, index) => <span key={index}>{item}</span>)}
       </div>
