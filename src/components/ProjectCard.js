14fd95b79fde9540baae2771326321ddced1071a
Shubhayan Srivastava <88150274+shubhayan935@users.noreply.github.com>
2024-07-11 15:34:26 +0530

    Update ProjectCard.js

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const ProjectCard = ({ title, image, description, highlight, tech, link }) => (
+const ProjectCard = ({ title, image, description, highlight, tech, link, demoLink }) => (
   <a href={link} className="project-card" target="_blank" rel="noopener noreferrer" style={{"textDecoration": "none"}}>
     <div className="project-info">
       <h2 className="project-title">{title}</h2>
+      {demoLink && (
+        <div className="view-demo">
+          <a href={demoLink} target="_blank" rel="noopener noreferrer">View Demo <i class="fas fa-link"></i></a>
+        </div>
+      )}
       <div className="project-thumbnail" style={{ backgroundImage: `url('${image}')` }}></div>
       <p className="project-description">{description}</p>
       {highlight && (
